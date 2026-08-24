519177686cfb047374b4c26260ae25c1ef5bb0fc
Eric Schultz <eric@startuperic.com>
2026-08-23 22:23:38 -0500

    Hoist per-pixel work out of the bitmap and quic conversion loops
    The loops re-read ret.data, recomputed the byte count, and re-tested
    the image format on every pixel.  Read them once outside the loop.
    
    Signed-off-by: Eric Schultz <eric@startuperic.com>

--- src/bitmap.js
+++ src/bitmap.js
@@ -42,18 +42,18 @@ function convert_spice_bitmap_to_web(context, spice_bitmap)
     }
 
     ret = context.createImageData(spice_bitmap.x, spice_bitmap.y);
-    for (offset = 0; offset < (spice_bitmap.y * spice_bitmap.stride); src_offset -= src_dec)
-        for (x = 0; x < spice_bitmap.x; x++, offset += 4, src_offset += 4)
+    var dest = ret.data;
+    var end = spice_bitmap.y * spice_bitmap.stride;
+    var w = spice_bitmap.x;
+    var opaque = spice_bitmap.format == Constants.SPICE_BITMAP_FMT_32BIT;
+    for (offset = 0; offset < end; src_offset -= src_dec)
+        for (x = 0; x < w; x++, offset += 4, src_offset += 4)
         {
-            ret.data[offset + 0 ] = u8[src_offset + 2];
-            ret.data[offset + 1 ] = u8[src_offset + 1];
-            ret.data[offset + 2 ] = u8[src_offset + 0];
-
+            dest[offset + 0] = u8[src_offset + 2];
+            dest[offset + 1] = u8[src_offset + 1];
+            dest[offset + 2] = u8[src_offset + 0];
             // FIXME - We effectively treat all images as having SPICE_IMAGE_FLAGS_HIGH_BITS_SET
-            if (spice_bitmap.format == Constants.SPICE_BITMAP_FMT_32BIT)
-                ret.data[offset + 3] = 255;
-            else
-                ret.data[offset + 3] = u8[src_offset];
+            dest[offset + 3] = opaque ? 255 : u8[src_offset];
         }
 
     return ret;
